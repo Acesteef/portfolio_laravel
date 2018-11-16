949e1b89596973e12e0c52db036a8b327ee4e79b
Stefan De Boer <stefandeboer502@gmail.com>
2018-11-16 11:08:23 +0100

    added scss compiler to the project.

--- public/index.php
+++ public/index.php
@@ -48,6 +48,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
+SassCompiler::run("scss/", "css/");
+// some people said this does not work in their installation, so if you have trouble, try these paths:
+// SassCompiler::run("public/scss/", "public/css/");
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -5,10 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Homepage</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="main.js"></script>
 </head>
 <body>
-    
+    <div class="welcome">
+        hey
+    </div>
 </body>
 </html>
